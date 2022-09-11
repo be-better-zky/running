0cb57df9ed8821d880d7ed0ca0ce6765c323af53
wy <1144571250@qq.com>
2022-09-11 20:19:03 +0800

    runningMessageZhou

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
+  pathPrefix: '/running', // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: '周观云的跑步记录',
     siteUrl: 'https://github.com/githubyun123',
